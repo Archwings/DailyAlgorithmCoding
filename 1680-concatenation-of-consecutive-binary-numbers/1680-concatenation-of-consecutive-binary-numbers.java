1010da0a7c454ff77d6e4a23e199062293de480a
Marvin Agonos <47682823+Archwings@users.noreply.github.com>
2022-09-23 14:00:17 -0700

    Time: 80 ms (100.00%), Space: 39.2 MB (94.81%) - LeetHub

---
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int concatenatedBinary(int n) {
+        long ans =  0;
+        int length = 0;
+        final int mod = 1000000007;
+        for (int i = 1; i <= n; i++ ) {
+            if ((i & (i-1)) == 0) {
+                length++; 
+            }
+            ans = ((ans << length) | i) % mod;
+        }
+        return (int) ans;
+    }
+}
