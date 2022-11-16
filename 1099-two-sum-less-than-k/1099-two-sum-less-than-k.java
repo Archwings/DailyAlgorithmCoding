d69dd6674d73115da2a4953f44186e077d9f4280
Marvin Agonos <47682823+Archwings@users.noreply.github.com>
2022-11-16 14:44:30 -0800

    Time: 44 ms (5.68%), Space: 43.6 MB (5.22%) - LeetHub

---
+++ 1099-two-sum-less-than-k/1099-two-sum-less-than-k.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int twoSumLessThanK(int[] nums, int k) {
+        // Sort the nums array. Using two pointers, one at index 0 and the other at index nums.length, move the pointers depending on the sum of the integers pointed to. Keep track of the highest sum under k.
+        // If the sum > k, move the right pointer by a index,
+        // If sum < k, move the left pointer by an index higher.
+        int biggestSum = -1;
+        Arrays.sort(nums);
+        System.out.println(Arrays.toString(nums));
+        int left = 0; 
+        int right = nums.length - 1;
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+            System.out.println(sum);
+            if (sum < k) {
+                biggestSum = Math.max(biggestSum, sum);
+                left++;
+            } else{
+                right--;
+            }
+        }
+        return biggestSum;
+    }
+}
